bc5e9fdb601006e3f7934f9f9a1a0cbe069cc5c9
jobyojna <jobyojnahubofficial@gmail.com>
2025-05-10 19:32:01 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
-api_id = "api id"
-api_hash = "api hadh"
-bot_token = "bot token"
-auth_users = [7890781002]
-sudo_users = [7890781002]
+api_id = "22182189"
+api_hash = "5e7c4088f8e23d0ab61e29ae11960bf5"
+bot_token = "7895084091:AAF8IYZWxIUgDqXdsqK1pMORrHg2es5xK0Q"
+auth_users = [5866664347]
+sudo_users = [5866664347]
